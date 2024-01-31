72796f5b58903035df183cb42783e20cbe6a83cd
SketchedDoughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-01-30 20:18:07 -0800

    z

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ class Functions:
 
     def clear_terminal(self):
         self.console._js.terminal.clear()
-
+    
     def toggle_terminal(self, event):
         hidden = self.console.parent._js.getAttribute("hidden")
         if hidden:
@@ -38,3 +38,34 @@ class Functions:
             self.console.parent._js.setAttribute("hidden", "hidden")
 
 FUNCTIONS = Functions()
+
+
+
+from pyweb import pydom
+
+class TicTacToe:
+    def __init__(self):
+        self.board = pydom["table#board"]
+        self.status = pydom["h2#status"]
+        self.console = pydom["script#console"][0]
+        self.init_cells()
+        self.init_winning_combos()
+        self.new_game(...)
+
+    def click(self, event):
+        i = int(event.target.getAttribute('data-x'))
+        j = int(event.target.getAttribute('data-y'))
+        print(f'Cell {i}, {j} clicked: ', end='')
+        return
+    
+    def clear_terminal(self):
+        self.console._js.terminal.clear()
+    
+    def toggle_terminal(self, event):
+        hidden = self.console.parent._js.getAttribute("hidden")
+        if hidden:
+            self.console.parent._js.removeAttribute("hidden")
+        else:
+            self.console.parent._js.setAttribute("hidden", "hidden")
+
+GAME = TicTacToe()

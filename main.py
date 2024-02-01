ea30b74c38e4901e404062876cdec482a9b0ca54
SketchedDoughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-02-01 18:32:02 +0000

    x

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
-from pyscript import document
+import pyscript
 import asyncio
+import threading
 
 # global variables
 global key
@@ -12,22 +13,30 @@ client = discord.Client(command_prefix="!", intents=intents)
 
 @client.event
 async def on_ready():
-    print('on ready')
+    output_div.innerText = 'bot is on ready'
 
 @client.event
 async def on_message(message):
     print(message)
 ###################################################################################################################################################
-
-async def grabInfo(event):
+#@pyscript_executor
+def grabInfo(event):
     global token
     global key
-    key = document.querySelector("#key")
+    global output_div
+    key = pyscript.document.querySelector("#key")
     key = key.value
-    token = document.querySelector("#token")
+    token = pyscript.document.querySelector("#token")
     token = token.value
-    output_div = document.querySelector("#output")
-    output_div.innerText = str(token) + str(key)
+    output_div = pyscript.document.querySelector("#output")
+    output_div.innerText = (f'{key}, {token}')
 
     # run the bot below
-    await asyncio.gather(client.run(token))
+    #await asyncio.gather(client.run(token))
+    client.run(token)
+
+def running(event):
+    t1 = threading.Thread(target=grabInfo,)
+    t1.start()
+
+test = 'MTE1MDg2NzE3OTk1NzQwNzkwNQ.GxeGJv.UMrOq2DyGHVDRoU9GI0CGNQYv1HudTJXp5OlnI'

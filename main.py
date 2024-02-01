646156630ac25e02970bf10f7d70d4b51224f883
SketchedDoughnut <102433598+SketchedDoughnut@users.noreply.github.com>
2024-02-01 20:22:35 +0000

    x

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import discord
 import pyscript
 import asyncio
-import threading
-import time
+import nest_asyncio
 
 # global variables
 global key
@@ -20,10 +19,10 @@ async def on_ready():
 async def on_message(message):
     print(message)
 
-def runBot():
-    global token
-    token = ' '
-    client.run(token)
+def runBot(token):
+    #nest_asyncio.apply(client.run(token))
+    #asyncio.get_event_loop().run_until_complete(client.run(token))
+    asyncio.create_task(client.run(token))
 ###################################################################################################################################################
 #@pyscript_executor
 def grabInfo(event):
@@ -36,9 +35,8 @@ def grabInfo(event):
     token = token.value
     output_div = pyscript.document.querySelector("#output")
     output_div.innerText = (f'key: {key} \n token: {token}')
-
+    #asyncio.get_event_loop().run_until_complete(runBot(token))
+    #nest_asyncio.apply()(r)
+    runBot(token)
     # loading = pyscript.document.querySelector("#loading")
     # loading.innerText = " "
-    runBot()
-    # run the bot below
-    #await asyncio.gather(client.run(token))
